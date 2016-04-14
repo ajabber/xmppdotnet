97473e7276d56674d9c97f0b454316bf4495ef9b
ajabber <menshchikov.m@bk.ru>
2016-04-14 12:08:44 +0300

    Create blackjabber.cc

---
+++ vcards/blackjabber.cc
@@ -0,0 +1,43 @@
+<vcard>
+  <fn>
+    <text>blackjabber.cc</text>
+  </fn>
+  <kind>
+    <text>application</text>
+  </kind>
+  <url>
+    <uri>http://blackjabber.cc/</uri>
+  </url>
+  <note>
+    <text>BlackJabber.cc :: Anonymous Jabber Server</text>
+  </note>
+  <registration xmlns='urn:xmpp:vcard:registration:1'>
+    <uri>xmpp:blackjabber.cc</uri>
+  </registration>
+  <bday>
+    <date>2016</date>
+  </bday>
+  <adr>
+    <country>NL</country>
+  </adr>
+  <ca xmlns='urn:xmpp:vcard:ca:0'>
+    <name>GlobalSign</name>
+    <uri>https://www.globalsign.com</uri>
+  </ca>
+  <name xmlns='jabber:iq:version'>ejabberd</name>
+  <impp>
+    <uri>xmpp:admin@blackjabber.cc</uri>
+  </impp>
+  <geo>
+    <uri>geo:52.230,4.540</uri>
+  </geo>
+  <tz>
+    <text>Europe/Amsterdam</text>
+  </tz>
+  <lang>
+    <parameters>
+      <pref>1</pref>
+    </parameters>
+    <language-tag>EN</language-tag>
+  </lang>
+</vcard>
